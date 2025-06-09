8fbcce3e9483b04fd8201718857f0e4cd90c3a8f
alexbic <alex_bic@mac.com>
2025-06-09 11:22:28 -0500

    ЧАТ #37: Восстановлена функция M.log в utils/debug.lua
    ИСПРАВЛЕНО:
    ✅ Добавлены отсутствующие 'end' для if и function
    ✅ Полная структура файла восстановлена
    ✅ Global логирование должно работать
    
    РЕЗУЛЬТАТ: [global] ⚡ переменные снова логируются

--- utils/debug.lua
+++ utils/debug.lua
@@ -1,24 +1,14 @@
--- cat > ~/.config/wezterm/utils/debug.lua << 'EOF'
---
--- ОПИСАНИЕ: Самодостаточная система управляемого отладочного логирования
--- Позволяет включать/выключать отладочные сообщения по модулям
--- ПОЛНОСТЬЮ САМОДОСТАТОЧНЫЙ МОДУЛЬ - все зависимости передаются как параметры.
---
--- ЗАВИСИМОСТИ: НЕТ
-
 local M = {}
 
--- Настройки отладки (включить/выключить по модулям)
 M.DEBUG_CONFIG = {
-  session_status = false,    -- отладка статуса сессий
-  appearance = false,        -- отладка внешнего вида
-  resurrect = false,         -- отладка сохранения/восстановления
-  workspace = false,         -- отладка workspace
-  bindings = false,          -- отладка горячих клавиш
-  global = true,            -- общая отладка
+  session_status = false,
+  appearance = false,
+  resurrect = false,
+  workspace = false,
+  bindings = false,
+  global = true,
 }
 
--- Простое логирование с поддержкой таблицы локализации
 M.log = function(wezterm, t_table, module, message_key, ...)
   if M.DEBUG_CONFIG[module] then
     local colors = require("config.environment.colors")
@@ -26,38 +16,13 @@ M.log = function(wezterm, t_table, module, message_key, ...)
     local env_utils = require("utils.environment")
     local localized_msg = (t_table and t_table[message_key]) or message_key
     local formatted_msg = string.format(localized_msg, ...)
-    local icon = icons.t[module] or "🔧"
-    local color_code = env_utils.get_ansi_color(colors, module)
     local var_icon = icons.t.set_env_var or "⚡"
     local var_color = env_utils.get_ansi_color(colors, "set_env_var")
-    local colored_prefix = "27[38;5;" .. var_color .. "m[" .. module .. "] " .. var_icon .. "27[0m"  end
-end
--- Выключить отладку для модуля  
-M.disable_debug = function(wezterm, t_table, module)
-  M.DEBUG_CONFIG[module] = false
-  local msg = (t_table and t_table["debug_disabled_for_module"]) or "Debug disabled for module: %s"
-  wezterm.log_info("⚙ " .. string.format(msg, module))
-end
-
--- Включить отладку для всех модулей
-M.enable_all = function(wezterm, t_table)
-  for module, _ in pairs(M.DEBUG_CONFIG) do
-    M.DEBUG_CONFIG[module] = true
+    local colored_prefix = "\27[38;5;" .. var_color .. "m[" .. module .. "] " .. var_icon .. "\27[0m"
+    wezterm.log_info(colored_prefix .. " " .. formatted_msg)
   end
-  local msg = (t_table and t_table["debug_all_enabled"]) or "All debug modules enabled"
-  wezterm.log_info("⚙ " .. msg)
 end
 
--- Выключить отладку для всех модулей
-M.disable_all = function(wezterm, t_table)
-  for module, _ in pairs(M.DEBUG_CONFIG) do
-    M.DEBUG_CONFIG[module] = false
-  end
-  local msg = (t_table and t_table["debug_all_disabled"]) or "All debug modules disabled"
-  wezterm.log_info("⚙ " .. msg)
-end
-
--- Функция загрузки настроек отладки из Lua файла
 M.load_debug_settings = function(wezterm)
   local settings_file = wezterm.config_dir .. "/session-state/debug-settings.lua"
   local file = io.open(settings_file, "r")
@@ -78,7 +43,6 @@ M.load_debug_settings = function(wezterm)
   end
 end
 
--- Функция сохранения настроек отладки в Lua файл
 M.save_debug_settings = function(wezterm)
   if not wezterm then
     wezterm = require('wezterm')
@@ -109,4 +73,5 @@ M.save_debug_settings = function(wezterm)
     file:close()
   end
 end
+
 return M
